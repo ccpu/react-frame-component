5b9230e064effd829f0b55b6a8c4bb00693991c8
cyrus <mdoaie@yahoo.co.uk>
2022-01-19 09:20:04 +0330

    Updates

--- appbundle.js
+++ appbundle.js
@@ -29368,6 +29368,7 @@
 	Object.defineProperty(exports, "__esModule", {
 	  value: true
 	});
+	exports.Frame = undefined;
 	
 	var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 	
@@ -29399,7 +29400,7 @@
 	
 	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 	
-	var Frame = function (_Component) {
+	var Frame = exports.Frame = function (_Component) {
 	  _inherits(Frame, _Component);
 	
 	  // React warns when you render directly into the body since browser extensions
@@ -29411,11 +29412,25 @@
 	
 	    var _this = _possibleConstructorReturn(this, (Frame.__proto__ || Object.getPrototypeOf(Frame)).call(this, props, context));
 	
+	    _this.setRef = function (node) {
+	      _this.nodeRef.current = node;
+	
+	      var forwardedRef = _this.props.forwardedRef; // eslint-disable-line react/prop-types
+	
+	      if (typeof forwardedRef === 'function') {
+	        forwardedRef(node);
+	      } else if (forwardedRef) {
+	        forwardedRef.current = node;
+	      }
+	    };
+	
 	    _this.handleLoad = function () {
-	      _this.forceUpdate();
+	      _this.setState({ iframeLoaded: true });
 	    };
 	
 	    _this._isMounted = false;
+	    _this.nodeRef = _react2.default.createRef();
+	    _this.state = { iframeLoaded: false };
 	    return _this;
 	  }
 	
@@ -29427,8 +29442,10 @@
 	      var doc = this.getDoc();
 	      if (doc && doc.readyState === 'complete') {
 	        this.forceUpdate();
-	      } else {
-	        this.node.addEventListener('load', this.handleLoad);
+	      }
+	
+	      if (doc) {
+	        this.nodeRef.current.contentWindow.addEventListener('DOMContentLoaded', this.handleLoad);
 	      }
 	    }
 	  }, {
@@ -29436,12 +29453,12 @@
 	    value: function componentWillUnmount() {
 	      this._isMounted = false;
 	
-	      this.node.removeEventListener('load', this.handleLoad);
+	      this.nodeRef.current.removeEventListener('load', this.handleLoad);
 	    }
 	  }, {
 	    key: 'getDoc',
 	    value: function getDoc() {
-	      return this.node ? this.node.contentDocument : null; // eslint-disable-line
+	      return this.nodeRef.current ? this.nodeRef.current.contentDocument : null; // eslint-disable-line
 	    }
 	  }, {
 	    key: 'getMountTarget',
@@ -29486,12 +29503,6 @@
 	        )
 	      );
 	
-	      if (doc.body.children.length < 1) {
-	        doc.open('text/html', 'replace');
-	        doc.write(this.props.initialContent);
-	        doc.close();
-	      }
-	
 	      var mountTarget = this.getMountTarget();
 	
 	      return [_reactDom2.default.createPortal(this.props.head, this.getDoc().head), _reactDom2.default.createPortal(contents, mountTarget)];
@@ -29499,9 +29510,8 @@
 	  }, {
 	    key: 'render',
 	    value: function render() {
-	      var _this2 = this;
-	
 	      var props = _extends({}, this.props, {
+	        srcDoc: this.props.initialContent,
 	        children: undefined // The iframe isn't ready so we drop children from props here. #12, #17
 	      });
 	      delete props.head;
@@ -29509,14 +29519,11 @@
 	      delete props.mountTarget;
 	      delete props.contentDidMount;
 	      delete props.contentDidUpdate;
+	      delete props.forwardedRef;
 	      return _react2.default.createElement(
 	        'iframe',
-	        _extends({}, props, {
-	          ref: function ref(node) {
-	            _this2.node = node;
-	          }
-	        }),
-	        this.renderFrameContents()
+	        _extends({}, props, { ref: this.setRef }),
+	        this.state.iframeLoaded && this.renderFrameContents()
 	      );
 	    }
 	  }]);
@@ -29542,7 +29549,9 @@
 	  contentDidUpdate: function contentDidUpdate() {},
 	  initialContent: '<!DOCTYPE html><html><head></head><body><div class="root"></div></body></html>'
 	};
-	exports.default = Frame;
+	exports.default = _react2.default.forwardRef(function (props, ref) {
+	  return _react2.default.createElement(Frame, _extends({}, props, { forwardedRef: ref }));
+	});
 
 /***/ },
 /* 22 */
